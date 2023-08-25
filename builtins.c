e08c57dfa85b2a60a3619e7f995ff5734e8dde00
Only1Denno <only1denno@gmail.com>
2023-08-25 10:51:44 -0700

    Terminal fixed

--- builtins.c
+++ builtins.c
@@ -1,12 +1,12 @@
 #include "simple_shell.h"
 int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
+int terminal_exit(char **args, char **front);
+int terminal_cd(char **args, char __attribute__((__unused__)) **front);
+int terminal_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
  * get_builtin - Matches a command with a corresponding
- *               shellby builtin function.
+ *               terminal builtin function.
  * @command: The command to be matched with.
  *
  * Return: A function that  points to the corresponding builtin.
@@ -14,13 +14,13 @@ int shellby_help(char **args, char __attribute__((__unused__)) **front);
 int (*get_builtin(char *command))(char **args, char **front)
 {
 	builtin_t funcs[] = {
-		{ "exit", shellby_exit },
+		{ "exit", terminal_exit },
 		{ "env", shellby_env },
 		{ "setenv", shellby_setenv },
 		{ "unsetenv", shellby_unsetenv },
-		{ "cd", shellby_cd },
+		{ "cd", terminal_cd },
 		{ "alias", shellby_alias },
-		{ "help", shellby_help },
+		{ "help", terminal_help },
 		{ NULL, NULL }
 	};
 	int i;
@@ -34,7 +34,7 @@ int (*get_builtin(char *command))(char **args, char **front)
 }
 
 /**
- * shellby_exit - Terminates the shellby shell.
+ * terminal_exit - Terminates the terminal shell.
  * @args: An array of arguments that contains the exit value.
  * @front: A double pointer to the beginning of args.
  *
@@ -44,7 +44,7 @@ int (*get_builtin(char *command))(char **args, char **front)
  *
  * Description: Upon returning -3, the program exits back in the main function.
  */
-int shellby_exit(char **args, char **front)
+int terminal_exit(char **args, char **front)
 {
 	int i, len_of_int = 10;
 	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
@@ -78,7 +78,7 @@ int shellby_exit(char **args, char **front)
 }
 
 /**
- * shellby_cd - Changes the current directory of the shellby process.
+ * terminal_cd - Changes the current directory of the terminal process.
  * @args: An array of arguments.
  * @front: A double pointer to the beginning of args.
  *
@@ -86,7 +86,7 @@ int shellby_exit(char **args, char **front)
  *         If an error occurs - -1.
  *         Otherwise - 0.
  */
-int shellby_cd(char **args, char __attribute__((__unused__)) **front)
+int terminal_cd(char **args, char __attribute__((__unused__)) **front)
 {
 	char **dir_info, *new_line = "\n";
 	char *oldpwd = NULL, *pwd = NULL;
@@ -159,14 +159,14 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 }
 
 /**
- * shellby_help - Displays information about shellby builtin commands.
+ * terminal_help - Displays information about terminal builtin commands.
  * @args: An array of arguments.
  * @front: Points to the beginning of args.
  *
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
  */
-int shellby_help(char **args, char __attribute__((__unused__)) **front)
+int terminal_help(char **args, char __attribute__((__unused__)) **front)
 {
 	if (!args[0])
 		help_all();
